b7ba6ee042d3c7fbfd127ab8deb17b8642870514
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-01-07 18:12:10 -0800

    Add button component

---
+++ client/src/components/submitButton.js
@@ -0,0 +1,7 @@
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+export default function SubmitButton() {
+    return (
+        <Button colorScheme='green' mt = "10px" mb="8px">Submit</Button>
+    )
+}
