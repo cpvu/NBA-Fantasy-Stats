10e5b5fde3804769961cd1d4b28212c02afa91b1
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-01-08 04:17:43 -0800

    Add fading for table on click

--- client/src/components/submitButton.js
+++ client/src/components/submitButton.js
@@ -1,7 +1,29 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Button, Fade, ScaleFade, Slide, SlideFade, useDisclosure, onToggle, Center, Container, VStack } from '@chakra-ui/react'
+import PlayerTable from "./table"
 
 export default function SubmitButton() {
+    const { isOpen, onToggle } = useDisclosure()
+
     return (
-        <Button colorScheme='orange' mt = "25px" mb="25px" size="lg">Submit</Button>
+       <Center>
+        <VStack>
+        <Button onClick={onToggle} colorScheme='orange' mt = "40px" mb="25px" size="lg">Submit</Button>
+            <ScaleFade initialScale={0.9} in={isOpen}>   
+            <Container
+                maxW="90%"
+                rounded='lg'
+                border='1px' borderColor='gray.200'
+                bg='gray.50'
+                my={{ base: 10, md: 10}}>
+
+                    <PlayerTable></PlayerTable>
+                </Container>
+            </ScaleFade>
+        </VStack>
+           
+       </Center>
+     
+    
+
     )
 }
