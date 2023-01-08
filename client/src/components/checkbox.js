a1e0192e3b4b34826f5efed018cdf3126758e40a
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-01-07 18:12:29 -0800

    Add checkbox padding

--- client/src/components/checkbox.js
+++ client/src/components/checkbox.js
@@ -3,7 +3,7 @@ import { Checkbox, CheckboxGroup, Stack } from '@chakra-ui/react'
 export default function PositionCheckBox() {
     return(
         <CheckboxGroup colorScheme='green' defaultValue={['naruto', 'kakashi']}>
-            <Stack spacing={[1, 5]} direction={['column', 'row']}>
+            <Stack spacing={[1, 5]} direction={['column', 'row']} my="20px">
                 <Checkbox value='PG'>PG</Checkbox>
                 <Checkbox value='PF'>PF</Checkbox>
                 <Checkbox value='SG'>SG</Checkbox>
