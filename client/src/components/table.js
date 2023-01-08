98969ca47f845d6f420324cec5cb08c2c1d1109c
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-01-08 04:17:55 -0800

    Adjust table styling

--- client/src/components/table.js
+++ client/src/components/table.js
@@ -8,55 +8,56 @@ import {
     Td,
     TableCaption,
     TableContainer,
-  } from '@chakra-ui/react'
+} from '@chakra-ui/react'
 
 export default function PlayerTable() {
 
-return (
-    <TableContainer pt="25px" pb="15px">
-        <Table variant='simple' size="sm">
-        <TableCaption mt="10">NBA 2022-2023 Season</TableCaption>
-        <Thead>
-            <Tr>
-            <Th>Player</Th>
-            <Th isNumeric>Games Played</Th>
-            <Th isNumeric>Mins</Th>
-            <Th>Points</Th>
-            <Th isNumeric>FG %</Th>
-            <Th>FT %</Th>
-            <Th>FTA %</Th>
-            <Th isNumeric>3PM</Th>
-            <Th isNumeric>3PMA</Th>
-            <Th isNumeric>Rebounds</Th>
-            <Th isNumeric>Assists</Th>
-            <Th isNumeric>Turn Overs</Th>
-            </Tr>
-        </Thead>
-        <Tbody>
-            <Tr>
-            <Td>inches</Td>
-            <Td>millimetres (mm)</Td>
-            <Td isNumeric>25.4</Td>
-            <Td isNumeric>25.4</Td>
-            </Tr>
-            <Tr>
-            <Td>feet</Td>
-            <Td>centimetres (cm)</Td>
-            <Td isNumeric>30.48</Td>
-            <Td isNumeric>25.4</Td>
-            </Tr>
-            <Tr>
-            <Td>yards</Td>
-            <Td>metres (m)</Td>
-            <Td isNumeric>0.91444</Td>
-            <Td isNumeric>25.4</Td>
-            </Tr>
-        </Tbody>
-        <Tfoot>
-            <Tr>
-            </Tr>
-        </Tfoot>
-        </Table>
-    </TableContainer>
-)
+    return (
+
+        <TableContainer pt="25px" pb="15px" w="100%">
+            <Table variant='simple' size="lg">
+                <TableCaption mt="10">NBA 2022-2023 Season</TableCaption>
+                <Thead>
+                    <Tr>
+                        <Th>Player</Th>
+                        <Th>Games Played</Th>
+                        <Th>Mins</Th>
+                        <Th>Points</Th>
+                        <Th >FG %</Th>
+                        <Th>FT %</Th>
+                        <Th>FTA %</Th>
+                        <Th>3PM</Th>
+                        <Th>3PMA</Th>
+                        <Th>Rebounds</Th>
+                        <Th >Assists</Th>
+                        <Th >Turn Overs</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    <Tr>
+                        <Td>inches</Td>
+                        <Td>millimetres (mm)</Td>
+                        <Td>25.4</Td>
+                        <Td>25.4</Td>
+                    </Tr>
+                    <Tr>
+                        <Td>feet</Td>
+                        <Td>centimetres (cm)</Td>
+                        <Td >30.48</Td>
+                        <Td >25.4</Td>
+                    </Tr>
+                    <Tr>
+                        <Td>yards</Td>
+                        <Td>metres (m)</Td>
+                        <Td >0.91444</Td>
+                        <Td>25.4</Td>
+                    </Tr>
+                </Tbody>
+                <Tfoot>
+                    <Tr>
+                    </Tr>
+                </Tfoot>
+            </Table>
+        </TableContainer>
+    )
 }
