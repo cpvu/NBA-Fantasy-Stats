0a377c7bd978bc1c254f61139cff3f4c3cdc389e
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-01-08 02:50:28 -0800

    Add team dropdown import

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,5 +3,6 @@ import FilterMenu from './filters'
 import PlayerTable from './table'
 import PositionCheckBox from './checkbox';
 import SubmitButton from './submitButton';
+import TeamDropdown from './teamDropdown';
 
-export { CallToActionWithAnnotation, FilterMenu, PlayerTable, PositionCheckBox, SubmitButton }
+export { CallToActionWithAnnotation, FilterMenu, PlayerTable, PositionCheckBox, SubmitButton, TeamDropdown }
