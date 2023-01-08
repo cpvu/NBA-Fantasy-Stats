a36c969b87151433e25b3d2181e2ed84fff12d9e
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-01-08 04:17:29 -0800

    Reformat and remove unused import modules

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,50 +1,44 @@
-import {
-    Box,
-    Container,
-    Text,
-    Button,
-    Heading,
-    Stack,
-    Icon,
-    useColorModeValue,
-    createIcon,
-  } from '@chakra-ui/react';
-  
-  import PageDivider from './divider';
-  
-  export default function CallToActionWithAnnotation() {
+import { Center, Image, Text, Heading, Stack } from '@chakra-ui/react';
+import PageDivider from './divider';
+import AD3 from '../assets/AD3.jpg'
+
+export default function CallToActionWithAnnotation() {
     return (
-      <>
-          <Stack
-            textAlign={'center'}
-            spacing={{ base: 8, md: 5 }}
-            py={{ base: 100, md: 6 }}>
-            <Heading
-                fontFamily={"Roboto"}
-                letterSpacing= {1.5}
-                fontWeight={600}
-                fontSize={{ base: '2xl', sm: '4xl', md: '5xl' }}
-                lineHeight={'110%'}>
-                PBCC NBA Basketball Fantasy Tracker<br />
-                
-                <PageDivider></PageDivider>
-            </Heading>
-            <Text as={'span'} color={'orange.400'} fontSize={{ base: '2xl', sm: '1xl', md: '1xl' }} fontFamily={"Roboto"} >
-                    A statistical comparison of fantasy NBA 
+        <>
+            <Stack
+                textAlign={'center'}
+                spacing={{ base: 8, md: 5 }}
+                py={{ base: 100, md: 6 }}>
+                <Heading
+                    fontFamily={"Roboto"}
+                    letterSpacing={1.5}
+                    fontWeight={600}
+                    fontSize={{ base: '2xl', sm: '4xl', md: '5xl' }}
+                    lineHeight={'110%'}>
+                    PBCC NBA Basketball Fantasy Tracker<br />
+
+                    <PageDivider></PageDivider>
+                </Heading>
+                <Text as={'span'} color={'orange.400'} fontSize={{ base: '2xl', sm: '1xl', md: '1xl' }} fontFamily={"Roboto"} >
+                    A statistical comparison of fantasy NBA players
                 </Text>
 
+                <Center>
+                    <Image w="30%" src={AD3} alt='AD Injured' />
+                </Center>
+
                 <Text color={'gray.500'} fontSize={"20px"} fontFamily={"Roboto"}>
-                Our goal is to allow you to determine the truck type that provides specific boosts in efficiency of delivering and further fulfills our goal of sustainability.
+                    Filter below based on position, stats and teams
                 </Text>
-            <Stack
-              direction={'column'}
-              spacing={3}
-              align={'center'}
-              alignSelf={'center'}
-              position={'relative'}>
+                <Stack
+                    direction={'column'}
+                    spacing={3}
+                    align={'center'}
+                    alignSelf={'center'}
+                    position={'relative'}>
+                </Stack>
             </Stack>
-          </Stack>
-  
-      </>
+
+        </>
     );
-  }
+}
