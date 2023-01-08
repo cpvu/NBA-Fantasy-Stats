26ef319db94318bb29c602693e42d08c7307b564
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-01-07 22:50:47 -0800

    Refactor imports for components

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import CallToActionWithAnnotation from './Hero';
+import FilterMenu from './filters'
+import PlayerTable from './table'
+import PositionCheckBox from './checkbox';
+import SubmitButton from './submitButton';
+
+export { CallToActionWithAnnotation, FilterMenu, PlayerTable, PositionCheckBox, SubmitButton }
