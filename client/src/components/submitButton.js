6c0a17589364e3b8dd42121e7cb9c02340cbcd32
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-01-08 14:12:03 -0800

    Fix formatting

--- client/src/components/submitButton.js
+++ client/src/components/submitButton.js
@@ -7,15 +7,9 @@ export default function SubmitButton() {
     return (
        <Center>
         <VStack>
-        <Button onClick={onToggle} colorScheme='orange' mt = "40px" mb="25px" size="lg">Submit</Button>
-            <ScaleFade initialScale={0.9} in={isOpen}>   
-            <Container
-                maxW="90%"
-                rounded='lg'
-                border='1px' borderColor='gray.200'
-                bg='gray.50'
-                my={{ base: 10, md: 10}}>
-
+        {isOpen ? <Button colorScheme='orange' mt = "40px" mb="10px" size="lg">Submit</Button> : <Button onClick={onToggle} colorScheme='orange' mt = "40px" mb="10px" size="lg">Submit</Button>}
+            <ScaleFade initialScale={0.9} in={isOpen}>
+                <Container maxW="90%" rounded='lg' border='1px' borderColor='gray.200' bg='gray.50' my={{ base: 10, md: 10}}>
                     <PlayerTable></PlayerTable>
                 </Container>
             </ScaleFade>
