cbda40e3226d77d037ca3099acaf3844cec023e9
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-01-07 17:58:34 -0800

    Add checkbox component

---
+++ client/src/components/checkbox.js
@@ -0,0 +1,15 @@
+import { Checkbox, CheckboxGroup, Stack } from '@chakra-ui/react'
+
+export default function PositionCheckBox() {
+    return(
+        <CheckboxGroup colorScheme='green' defaultValue={['naruto', 'kakashi']}>
+            <Stack spacing={[1, 5]} direction={['column', 'row']}>
+                <Checkbox value='PG'>PG</Checkbox>
+                <Checkbox value='PF'>PF</Checkbox>
+                <Checkbox value='SG'>SG</Checkbox>
+                <Checkbox value='SF'>SF</Checkbox>
+                <Checkbox value='C'>C</Checkbox>
+            </Stack>
+        </CheckboxGroup>
+    )
+}
