f7ee8094416f86ff2618f05e73ffa3228bc026de
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-01-08 17:41:01 -0800

    Adjust container colors

--- client/src/components/submitButton.js
+++ client/src/components/submitButton.js
@@ -9,7 +9,7 @@ export default function SubmitButton() {
         <VStack>
         {isOpen ? <Button colorScheme='orange' mt = "40px" mb="10px" size="lg">Submit</Button> : <Button onClick={onToggle} colorScheme='orange' mt = "40px" mb="10px" size="lg">Submit</Button>}
             <ScaleFade initialScale={0.9} in={isOpen}>
-                <Container maxW="90%" rounded='lg' border='1px' borderColor='gray.200' bg='gray.50' my={{ base: 10, md: 10}}>
+                <Container maxW="90%" rounded='lg' border='1px' borderColor='gray.300' bg="gray.200" my={{ base: 10, md: 10}}>
                     <PlayerTable></PlayerTable>
                 </Container>
             </ScaleFade>
