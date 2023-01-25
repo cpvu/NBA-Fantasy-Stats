96d281a91a5a2510cb1e63114bd0dcc793b6b2e3
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-01-25 15:00:04 -0800

    Add object data for request

--- client/src/components/submitButton.js
+++ client/src/components/submitButton.js
@@ -6,12 +6,16 @@ export default function SubmitButton(props) {
     const { isOpen, onToggle } = useDisclosure()
 
     const [ playerInformation, setPlayerInformation ] = React.useState([]); 
+
+    const checkboxData = {
+        playerStats: props.playerStats,
+        playerPositions: props.playerPositions,
+    }
     
-    function checktest() {
-        console.log(props.checkedItems)
-        fetch("http://localhost:8000/message", {
+    function submitRequest() {
+        fetch("http://localhost:8000/getStats", {
             method: 'POST',
-            body: JSON.stringify(props.checkedItems),
+            body: JSON.stringify(checkboxData),
             headers: {
                 'Content-Type': 'application/json'
             }
@@ -25,7 +29,7 @@ export default function SubmitButton(props) {
        <Center>
         <VStack>
         <form>
-            {isOpen ? <Button onClick={checktest} colorScheme='orange' mt = "40px" mb="10px" size="lg">Submit</Button> : <Button onClick={() => {checktest(); onToggle();}} colorScheme='orange' mt = "40px" mb="10px" size="lg">Submit</Button>}
+            {isOpen ? <Button onClick={submitRequest} colorScheme='orange' mt = "40px" mb="10px" size="lg">Submit</Button> : <Button onClick={() => {submitRequest(); onToggle();}} colorScheme='orange' mt = "40px" mb="10px" size="lg">Submit</Button>}
         </form>
             <ScaleFade initialScale={0.9} in={isOpen}>
                 <Container maxW="90%" rounded='lg' border='1px' borderColor='gray.300' bg="gray.200" my={{ base: 10, md: 10}}>
