81ca3094bc268034bf305ed35679af65bcf38a07
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-01-25 13:03:10 -0800

    Add function to add/remove checkbox values

--- client/src/components/checkbox.js
+++ client/src/components/checkbox.js
@@ -1,9 +1,22 @@
-import { Checkbox, CheckboxGroup, useTheme, Text, Container, VStack, StackDivider, Grid, GridItem } from '@chakra-ui/react'
+import React from "react";
+import { Checkbox, CheckboxGroup, useTheme, Text, Container, VStack, StackDivider, Grid, GridItem, useCheckbox, useCheckboxGroup, getCheckboxprops } from '@chakra-ui/react'
 
 export default function PositionCheckBox(props) {
     const theme = useTheme()
     const playerPositions = props.checkboxValue
 
+    function callCheckbox(e,value) {
+        console.log(props.checkedItems)
+        props.setCheckedItems(prevItems => {
+           if(e.target.checked){
+              return [...prevItems, value];
+           }
+           else{
+              return prevItems.filter(item => item !== value);
+           }
+        });
+    }
+    
     return (
         <Container>
             <VStack divider={<StackDivider borderColor='white.200' />}>
@@ -11,13 +24,13 @@ export default function PositionCheckBox(props) {
                 <CheckboxGroup colorScheme='orange' defaultValue={[]}>
                     <Grid templateColumns='repeat(5, 1fr)' gap={3}>
                         {playerPositions?.map((x, i) => {
-                            return <GridItem><Checkbox key={i} value={x[i]}>{x}</Checkbox></GridItem>
-
+                            return <GridItem><Checkbox onChange={e => callCheckbox(e,x)} key={i} value={x[i]}>{x}</Checkbox>
+                            </GridItem>
                         })}
                     </Grid>
                 </CheckboxGroup>
             </VStack>
         </Container>
-
     )
 }
+
