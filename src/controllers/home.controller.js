01c9ea6e2fb4f1a5fbf05c420b8155f917564ecd
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-01-25 15:00:35 -0800

    Add filter for positions in getPlayerStats route

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,7 +1,25 @@
 const path = require('path');
+const playerJSON = require('../utils/player_stats.json')
 
 const root = path.join(__dirname, "..", "build")
 
 exports.getHomePage = (_req, res) => {
   res.sendFile(root, "index.html")
 };
+
+exports.getPlayerStats = (req, res) => {
+  const filters = req.body; 
+
+  const newPlayerObject = playerJSON.filter(x => {
+      for(let i = 0; i < filters.playerPositions.length; i++) {
+         if (x['Pos'].includes(filters.playerPositions[i])){
+            return x 
+         }
+      }
+  })
+
+  console.log(newPlayerObject)
+
+  res.send(newPlayerObject);
+}
+
